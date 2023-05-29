b88b26da8c77f4a453c747d77f2036308f9b1248
FRAALES <alessandrofranco1992@gmail.com>
2023-05-29 16:19:59 +0200

    riesco a stampare le 10 mail ma non con il ciclo for

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            email: '',
+            email1: '',
+            email2: '',
+            email3: '',
+            email4: '',
+            email5: '',
+            email6: '',
+            email7: '',
+            email8: '',
+            email9: '',
+            email10: '',
         }
     },
 
@@ -13,8 +22,33 @@ createApp({
         .then( 
             response => {
                 console.log(response.data.response);
-            this.email = response.data.response;
+            this.email1 = response.data.response;
             });
+
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( 
+            response => {
+                console.log(response.data.response);
+            this.email2 = response.data.response;
+            });
+
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( 
+            response => {
+                console.log(response.data.response);
+            this.email3 = response.data.response;
+            });
+        
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( 
+            response => {
+                console.log(response.data.response);
+            this.email4 = response.data.response;
+            });
+            
     }
 }) .mount('#app');
 
