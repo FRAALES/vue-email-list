7d8c1321e5db46e25992ce428257d1c9abaaf2e8
FRAALES <alessandrofranco1992@gmail.com>
2023-05-29 15:49:42 +0200

    v-for inizio a impostarlo

--- js/main.js
+++ js/main.js
@@ -3,8 +3,22 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            
-
+            emails: [
+                
+            ]
         }
+    },
+
+    mounted() {
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(function (response)
+            {
+            const result = response.data;
+            });
     }
 }) .mount('#app');
+
+
+//generare 10 indirizzi email 
+// stamparli in pagina all’interno di una lista.
